cb2405db52d968ff867e678c9966c0f8b0ecbf14
Eddie Offermann <eoffermann@gmail.com>
2026-04-10 20:54:45 -0700

    feat(bolt): simple-shape checker for graph projection (B3)

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,4 +2,5 @@ export * from './constants/index.js';
 export * from './schemas/index.js';
 export * from './types/index.js';
 export * from './bolt-graph.js';
+export * from './bolt-graph-shape.js';
 export * from './bolt-automation-versions/index.js';
